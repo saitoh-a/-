5e32903cc41d68596a9b4bc51d2dac912eef5e21
taken <taken@192.168.10.111>
2026-01-14 21:46:21 +0900

    Java3 導入

---
+++ HelloWorld_SaitohAyaka/src/sample/Selfintroduction.java
@@ -0,0 +1,29 @@
+package sample;
+
+public class Selfintroduction {
+
+	public static void main(String[] args) {
+		//名前
+		String name = "齋藤 彩香";
+		
+		//年齢
+		int age = 30;
+		
+		//趣味
+		String hobby = "猫吸い";
+		
+		
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
